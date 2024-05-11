d931dae2a6a5c27e3185e559752090a5bac27bad
Muhammad Enamul Haque <muhammad.enamul.haque94@gmail.com>
2024-05-12 00:09:32 +0600

    yes i can

--- es6Exploring.js
+++ es6Exploring.js
@@ -1,12 +1,7 @@
 //Step-1:
 document.getElementById("btn-1").addEventListener('click',function(){
-    // console.log('btn-1 clicked');
     const friends = document.getElementsByClassName("friends");
-    // console.log(friends);
-    // console.log(typeof friends);
     for(const friend of friends){
-        // console.log(friend);
-        // console.log(typeof friend);
         friend.style.backgroundColor = 'lightgray';
     }
    
@@ -14,18 +9,14 @@ document.getElementById("btn-1").addEventListener('click',function(){
 
 //Step-2:
 document.getElementById("btn-2").addEventListener("click",function(){
-    // console.log("btn-2 clicked");
     const friend = document.getElementById("friend-2");
-    // console.log(friend);
     friend.style.backgroundColor = 'seagreen';
     friend.style.color = 'white';
 })
 
 //Step-3:
 document.getElementById("btn-3").addEventListener("click",function(){
-    // console.log("btn-3 clicked");
     const divContainer = document.getElementById("divContainer");
-    // console.log(divContainer);
     const div = document.createElement("div");
     div.classList.add("friends");
     div.innerHTML =`
